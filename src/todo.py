d836c88e7aff786e592a30fcd8180c19b88278ff
Yanna01santos <135857226+Yanna01santos@users.noreply.github.com>
2025-11-08 14:27:28 -0300

    🔴test: cria teste inicial para adicionar tarefas no (RED)

--- src/todo.py
+++ src/todo.py
@@ -1,2 +1,20 @@
 from src.todo import ToDoList
-import pytest
+
+def test_adicionar_tarefa_cria_uma_tarefa():
+    """
+    Cenário:
+    - Quando eu crio uma lista de tarefas
+    - E adiciono uma tarefa com título válido
+    - Então a lista deve ter exatamente 1 tarefa
+    """
+    lista = ToDoList()  # ainda não existe -> teste vai falhar primeiro (RED)
+    lista.adicionar_tarefa("Estudar TDD")
+
+    tarefas = ToDoList()  # ainda não existe -> teste vai falhar primeiro (R
+    lista.adicionar_tarefa("Estudar TDD")
+
+    tarefas = lista.listar_tarefas()
+
+    assert len(tarefas) == 1
+    assert tarefas[0]["titulo"] == "Estudar TDD"
+    assert tarefas[0]["status"] == "pendente"
